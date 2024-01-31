438e514d15c29407737aac4c633dddd55acb7519
Simon Inikhov <semikinihov@gmail.com>
2024-01-31 13:26:17 +0300

    add test

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -1,15 +1,51 @@
 import unittest
+
+
 from utils import arrs
 
 
-class TestArrs(unittest.TestCase):
+class TestGet(unittest.TestCase):
 
-    def test_get(self):
+    def test_get_valid_index(self):
         self.assertEqual(arrs.get([1, 2, 3], 1, "test"), 2)
-        self.assertEqual(arrs.get([], 0, "test"), "test")
 
-    def test_slice(self):
+    def test_get_index_out_of_bounds(self):
+        self.assertEqual(arrs.get([1, 2, 3], 5, "test"), "test")
+
+    def test_get_negative_index(self):
+        self.assertEqual(arrs.get([1, 2, 3], -1, "test"), "test")
+
+    def test_get_empty_array(self):
+        self.assertEqual(arrs.get([], 0, "empty"), "empty")
+
+
+class TestMySlice(unittest.TestCase):
+
+    def test_slice_normal(self):
         self.assertEqual(arrs.my_slice([1, 2, 3, 4], 1, 3), [2, 3])
-        self.assertEqual(arrs.my_slice([1, 2, 3], 1), [2, 3])
+
+    def test_slice_with_start_and_end(self):
+        self.assertEqual(arrs.my_slice([1, 2, 3, 4], 1, -1), [2, 3])
+
+    def test_slice_with_start_only(self):
+        self.assertEqual(arrs.my_slice([1, 2, 3, 4], 2), [3, 4])
+
     def test_slice_negative_indices(self):
         self.assertEqual(arrs.my_slice([1, 2, 3, 4], -3, -1), [2, 3])
+
+    def test_slice_out_of_bounds_indices(self):
+        self.assertEqual(arrs.my_slice([1, 2, 3, 4], 5, 10), [])
+
+    def test_slice_empty_array(self):
+        self.assertEqual(arrs.my_slice([], 1, 3), [])
+
+    def test_slice_negative_start(self):
+        self.assertEqual(arrs.my_slice([1, 2, 3, 4], -2), [3, 4])
+
+    def test_slice_negative_start_and_end(self):
+        self.assertEqual(arrs.my_slice([1, 2, 3, 4], -3, -1), [2, 3])
+
+
+
+
+
